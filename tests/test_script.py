8fba89fc32194fadb2bff88c54f5a13f559d3bca
leechwork3-pixel <leechwork3@gmail.com>
2025-08-03 09:15:18 +0530

    Update test_script.py

--- tests/test_script.py
+++ tests/test_script.py
@@ -1,1 +1,15 @@
+import requests
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("TEST_CHAT_ID")
+
+def test_start():
+    r = requests.post(
+        f"https://api.telegram.org/bot{TOKEN}/sendMessage",
+        json={"chat_id": CHAT_ID, "text": "/start"}
+    )
+    assert r.status_code == 200
+  
