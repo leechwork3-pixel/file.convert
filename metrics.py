a77726d26f46b6480d3e2ce3c48a0d0b6ee3a7dc
leechwork3-pixel <leechwork3@gmail.com>
2025-08-03 09:16:49 +0530

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -1,1 +1,13 @@
+from flask import Flask
+from prometheus_flask_exporter import PrometheusMetrics
 
+app = Flask(__name__)
+metrics = PrometheusMetrics(app)
+
+@app.route("/")
+def home():
+    return "Metrics running..."
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
+  
