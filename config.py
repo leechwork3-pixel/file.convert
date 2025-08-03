c5e452064cbe0655bf2ed8d65cb673dcb74d0df2
leechwork3-pixel <leechwork3@gmail.com>
2025-08-03 10:03:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,12 +3,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-LOG_CHANNEL = int(os.getenv("LOG_CHANNEL"))
-MONGO_URI = os.getenv("MONGO_URI")
-SUDO_ADMINS = list(map(int, os.getenv("SUDO_ADMINS", "").split()))
+API_ID = int(os.getenv("24171111"))
+API_HASH = os.getenv("c850cb56b64b6c3b10ade9c28ef7966a")
+BOT_TOKEN = os.getenv("7804274444:AAESKpYJQVhftykvv5cKZP2uyCYvxlwQvow")
+LOG_CHANNEL = int(os.getenv("-1002585029413"))
+MONGO_URI = os.getenv("mongodb+srv://Furina:furinafile@furinafile.tjrqfwh.mongodb.net/?retryWrites=true&w=majority&appName=Furinafile")
+SUDO_ADMINS = list(map(int, os.getenv("SUDO_ADMINS", "1335306418").split()))
 
 DOWNLOAD_DIR = "downloads"
 MAX_FILE_SIZE_MB = 200
